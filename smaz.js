7d40e6aa2ade831152637fe4e3d4f12bd27425c7
John <je.llyb..ean.s...and.jo..h.n+github@gmail.com>
2012-02-16 08:38:51 -0800

    ..update..

--- smaz.js
+++ smaz.js
@@ -6,27 +6,7 @@ var smaz = {
   
   // Reverse compression codebook, decompression
   reverse_codebook: [
-  " ", "the", "e", "t", "a", "of", "o", "and", "i", "n", "s", "e ", "r", " th",
-  " t", "in", "he", "th", "h", "he ", "to", "\r\n", "l", "s ", "d", " a", "an",
-  "er", "c", " o", "d ", "on", " of", "re", "of ", "t ", ", ", "is", "u", "at",
-  "   ", "n ", "or", "which", "f", "m", "as", "it", "that", "\n", "was", "en",
-  "  ", " w", "es", " an", " i", "\r", "f ", "g", "p", "nd", " s", "nd ", "ed ",
-  "w", "ed", "http://", "for", "te", "ing", "y ", "The", " c", "ti", "r ", "his",
-  "st", " in", "ar", "nt", ",", " to", "y", "ng", " h", "with", "le", "al", "to ",
-  "b", "ou", "be", "were", " b", "se", "o ", "ent", "ha", "ng ", "their", "\"",
-  "hi", "from", " f", "in ", "de", "ion", "me", "v", ".", "ve", "all", "re ",
-  "ri", "ro", "is ", "co", "f t", "are", "ea", ". ", "her", " m", "er ", " p",
-  "es ", "by", "they", "di", "ra", "ic", "not", "s, ", "d t", "at ", "ce", "la",
-  "h ", "ne", "as ", "tio", "on ", "n t", "io", "we", " a ", "om", ", a", "s o",
-  "ur", "li", "ll", "ch", "had", "this", "e t", "g ", "e\r\n", " wh", "ere",
-  " co", "e o", "a ", "us", " d", "ss", "\n\r\n", "\r\n\r", "=\"", " be", " e",
-  "s a", "ma", "one", "t t", "or ", "but", "el", "so", "l ", "e s", "s,", "no",
-  "ter", " wa", "iv", "ho", "e a", " r", "hat", "s t", "ns", "ch ", "wh", "tr",
-  "ut", "/", "have", "ly ", "ta", " ha", " on", "tha", "-", " l", "ati", "en ",
-  "pe", " re", "there", "ass", "si", " fo", "wa", "ec", "our", "who", "its", "z",
-  "fo", "rs", ">", "ot", "un", "<", "im", "th ", "nc", "ate", "><", "ver", "ad",
-  " we", "ly", "ee", " n", "id", " cl", "ac", "il", "</", "rt", " wi", "div",
-  "e, ", " it", "whi", " ma", "ge", "x", "e c", "men", ".com"],
+  " ", "the", "e", "t", "a", "of", "o", "and", "i", "n", "s", "e ", "r", " th", " t", "in", "he", "th", "h", "he ", "to", "\r\n", "l", "s ", "d", " a", "an","er", "c", " o", "d ", "on", " of", "re", "of ", "t ", ", ", "is", "u", "at", "   ", "n ", "or", "which", "f", "m", "as", "it", "that", "\n", "was", "en", "  ", " w", "es", " an", " i", "\r", "f ", "g", "p", "nd", " s", "nd ", "ed ", "w", "ed", "http://", "for", "te", "ing", "y ", "The", " c", "ti", "r ", "his", "st", " in", "ar", "nt", ",", " to", "y", "ng", " h", "with", "le", "al", "to ", "b", "ou", "be", "were", " b", "se", "o ", "ent", "ha", "ng ", "their", "\"", "hi", "from", " f", "in ", "de", "ion", "me", "v", ".", "ve", "all", "re ", "ri", "ro", "is ", "co", "f t", "are", "ea", ". ", "her", " m", "er ", " p", "es ", "by", "they", "di", "ra", "ic", "not", "s, ", "d t", "at ", "ce", "la", "h ", "ne", "as ", "tio", "on ", "n t", "io", "we", " a ", "om", ", a", "s o", "ur", "li", "ll", "ch", "had", "this", "e t", "g ", "e\r\n", " wh", "ere", " co", "e o", "a ", "us", " d", "ss", "\n\r\n", "\r\n\r", "=\"", " be", " e", "s a", "ma", "one", "t t", "or ", "but", "el", "so", "l ", "e s", "s,", "no", "ter", " wa", "iv", "ho", "e a", " r", "hat", "s t", "ns", "ch ", "wh", "tr", "ut", "/", "have", "ly ", "ta", " ha", " on", "tha", "-", " l", "ati", "en ", "pe", " re", "there", "ass", "si", " fo", "wa", "ec", "our", "who", "its", "z", "fo", "rs", ">", "ot", "un", "<", "im", "th ", "nc", "ate", "><", "ver", "ad", " we", "ly", "ee", " n", "id", " cl", "ac", "il", "</", "rt", " wi", "div", "e, ", " it", "whi", " ma", "ge", "x", "e c", "men", ".com"],
 
   flush_verbatim: function(verbatim) {
     var output = [];
@@ -113,7 +93,7 @@ var smaz = {
       } else if(input[i] === 255) {
         // Verbatim string
         var j;
-        if(i+input[i+1]+1 >= input.length) {
+        if(i+input[i+1]+2 >= input.length) {
           throw "Malformed smaz.";
         }
         for(j = 0; j < input[i+1]+1; j++) {
